638b957b421ba51f154f549679196a7e25e71383
vasilisdm <vasilisdmr@gmail.com>
2019-08-26 22:21:32 +0300

    Added the index method which returns a view, and provide that view with the name of the controller and the method.

--- UrlsAndRoutes/Controllers/AdminController.cs
+++ UrlsAndRoutes/Controllers/AdminController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
 namespace UrlsAndRoutes.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
-        public AdminController()
-        {
-        }
+        public ViewResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(AdminController),
+                Action = nameof(Index)
+            });
     }
 }
