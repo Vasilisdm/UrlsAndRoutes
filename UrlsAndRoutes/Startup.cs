9787dd3748d107d382899b3329c6f866a99265fe
vasilisdm <vasilisdmr@gmail.com>
2019-09-01 14:29:15 +0300

    Created the outbound route in order to generate ulr in a specific format.

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -39,7 +39,12 @@ namespace UrlsAndRoutes
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}"
-                ); 
+                );
+
+                routes.MapRoute(
+                    name: "Outbound",
+                    template: "outbound/{controller=Home}/{action=Index}"
+                );
             });
         }
     }
