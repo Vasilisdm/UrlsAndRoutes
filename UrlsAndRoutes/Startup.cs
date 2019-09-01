50caa705d1f768be03a43d66805c61264fa25ef5
vasilisdm <vasilisdmr@gmail.com>
2019-09-01 19:10:51 +0300

    Because MapRoute doesn't have support custom routing classes, I had to use Routes.Add to register the old urls, which I want to support.

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -35,11 +35,10 @@ namespace UrlsAndRoutes
             app.UseStaticFiles();
             app.UseMvc(routes => {
 
-                //routes.MapRoute(
-                //    name: "NewRoute",
-                //    template: "App/Do{action}",
-                //    defaults: new { Controller = "Home" }
-                //);
+                routes.Routes.Add(new LegacyRoute (
+                   "/articles/Windows_3.1_Overview.html",
+                   "/old/.NET_1.0_Class_Library"
+               ));
 
                 routes.MapRoute(
                     name: "default",
